a2948052d1de1ce41b44daa69085163fb5ea4986
Travis CI <travis@ci.org>
2018-02-19 13:12:56 +0000

    Add new build data from https://github.com/GraphGrail/landing.git frontend 7be434d commit to master

--- js/script.js
+++ js/script.js
@@ -236,31 +236,32 @@ $('.constructormokap .slider2').slick({
     });
 
 
-//$('.lang-menu').width($('.lang-menu ul li:first-child').width()+12);
+    //$('.lang-menu').width($('.lang-menu ul li:first-child').width()+12);
 
-$(document).on("click",'.lang-menu a',function(){
-   $(this).parents('.lang-menu').toggleClass("open");
-   var clickedId = $(this).attr("id");
-   var activeId = $('.lang-menu li:first-child a').attr("id");
-   var first = $('.lang-menu li:first-child').html();
-   var currentLi = $('.lang-menu a[id$="'+clickedId+'"]').closest("li");
+    $(document).on("click",'.lang-menu a',function(){
+       $(this).parents('.lang-menu').toggleClass("open");
+       var clickedId = $(this).attr("id");
+       var activeId = $('.lang-menu li:first-child a').attr("id");
+       var first = $('.lang-menu li:first-child').html();
+       var currentLi = $('.lang-menu a[id$="'+clickedId+'"]').closest("li");
 
-   var current = currentLi.html();
-  if(clickedId == activeId) {
-     return false;
-     }
-   else{
-     $('.lang-menu').each(function(){
-            $(this).find('li:first-child').html(current);
-     });
-   currentLi.each(function(){
-      $(this).html(first);
+       var current = currentLi.html();
+      if(clickedId == activeId) {
+         return false;
+         }
+       else{
+         $('.lang-menu').each(function(){
+                $(this).find('li:first-child').html(current);
+         });
+       currentLi.each(function(){
+          $(this).html(first);
+        });
+       var currentHref = $('.lang-menu li:first-child a').attr("href");
+        location.href = currentHref; //  переход на другой язык
+       }
+    return false;
     });
-   var currentHref = $('.lang-menu li:first-child a').attr("href");
-    location.href = currentHref; //  переход на другой язык
-   }
-return false;
-});
+
 
 /* points scroll*/
 var a = 0;
